a2feff7c24ca71298a0c834ddf1187056666c23d
ivan <ivan@bukalapak.com>
2019-02-24 12:50:53 +0700

    appointmentsssss

--- app/web/main.go
+++ app/web/main.go
@@ -7,15 +7,16 @@ import (
 	"net/http"
 	"os"
 	"reflect"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/context"
+	"github.com/jinzhu/now"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+	"github.com/subosito/gotenv"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-
-	"github.com/subosito/gotenv"
 )
 
 // Errors
@@ -175,8 +176,10 @@ func wrapHandler(h http.Handler) httprouter.Handle {
 // Repo Office
 
 type Office struct {
-	Id   bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
-	Name string        `json:"name"`
+	Id        bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+	Name      string        `json:"name"`
+	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
 }
 
 type OfficesCollection struct {
@@ -213,6 +216,7 @@ func (r *OfficeRepo) Find(id string) (OfficeResource, error) {
 
 func (r *OfficeRepo) Create(office *Office) error {
 	id := bson.NewObjectId()
+	office.CreatedAt = time.Now()
 	_, err := r.coll.UpsertId(id, office)
 	if err != nil {
 		return err
@@ -224,6 +228,7 @@ func (r *OfficeRepo) Create(office *Office) error {
 }
 
 func (r *OfficeRepo) Update(office *Office) error {
+	office.UpdatedAt = time.Now()
 	err := r.coll.UpdateId(office.Id, office)
 	if err != nil {
 		return err
@@ -309,9 +314,11 @@ func (c *appContext) deleteOfficeHandler(w http.ResponseWriter, r *http.Request)
 // Repo Room
 
 type Room struct {
-	Id       bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
-	Name     string        `json:"name"`
-	OfficeId string        `json:office_id`
+	Id        bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+	Name      string        `json:"name"`
+	OfficeId  string        `json:"office_id"`
+	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
 }
 
 type RoomsCollection struct {
@@ -348,6 +355,7 @@ func (r *RoomRepo) Find(id string) (RoomResource, error) {
 
 func (r *RoomRepo) Create(room *Room) error {
 	id := bson.NewObjectId()
+	room.CreatedAt = time.Now()
 	_, err := r.coll.UpsertId(id, room)
 	if err != nil {
 		return err
@@ -359,6 +367,7 @@ func (r *RoomRepo) Create(room *Room) error {
 }
 
 func (r *RoomRepo) Update(room *Room) error {
+	room.UpdatedAt = time.Now()
 	err := r.coll.UpdateId(room.Id, room)
 	if err != nil {
 		return err
@@ -456,6 +465,177 @@ func (c *appContext) roomsOfficeHandler(w http.ResponseWriter, r *http.Request) 
 	WriteSuccess(w, http.StatusOK, rooms)
 }
 
+// Repo Appointment
+
+type Appointment struct {
+	Id              bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+	AppointmentName string        `json:"appointment_name"`
+	RoomId          string        `json:"room_id"`
+	AppointmentDesc string        `json:"appointment_description"`
+	GuestIds        []string      `json:"guest_ids"`
+	Owner           bool          `json:"owner"`
+	StartTime       time.Time     `json:"start_time"`
+	EndTime         time.Time     `json:"end_time"`
+	CreatedAt       time.Time     `json:"created_at"`
+	UpdatedAt       time.Time     `json:"updated_at"`
+}
+
+type AppointmentsCollection struct {
+	Data []Appointment `json:"data"`
+}
+
+type AppointmentResource struct {
+	Data Appointment `json:"data"`
+}
+
+type AppointmentRepo struct {
+	coll *mgo.Collection
+}
+
+func (r *AppointmentRepo) All() (AppointmentsCollection, error) {
+	result := AppointmentsCollection{[]Appointment{}}
+	beginningOfMonth := now.BeginningOfMonth()
+	endOfMonth := now.EndOfMonth()
+	err := r.coll.Find(bson.M{"start_time": bson.M{"$gte": beginningOfMonth, "$lte": endOfMonth}}).All(&result.Data)
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
+
+func (r *AppointmentRepo) Find(id string) (AppointmentResource, error) {
+	result := AppointmentResource{}
+	err := r.coll.FindId(bson.ObjectIdHex(id)).One(&result.Data)
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
+
+func (r *AppointmentRepo) Create(appointment *Appointment) error {
+	id := bson.NewObjectId()
+	appointment.CreatedAt = time.Now()
+	_, err := r.coll.UpsertId(id, appointment)
+	if err != nil {
+		return err
+	}
+
+	appointment.Id = id
+
+	return nil
+}
+
+func (r *AppointmentRepo) Update(appointment *Appointment) error {
+	appointment.UpdatedAt = time.Now()
+	err := r.coll.UpdateId(appointment.Id, appointment)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *AppointmentRepo) Delete(id string) error {
+	err := r.coll.RemoveId(bson.ObjectIdHex(id))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *AppointmentRepo) Search(roomIds []string, owner bool, guestId string) (AppointmentsCollection, error) {
+	result := AppointmentsCollection{[]Appointment{}}
+	err := r.coll.Find(bson.M{
+		"room_id": bson.M{
+			"$in": roomIds,
+		}, "$or": []bson.M{
+			{"owner": owner},
+			{"guest_ids": guestId},
+		},
+	}).All(&result.Data)
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
+
+// Appointment Handlers
+
+func (c *appContext) appointmentsHandler(w http.ResponseWriter, r *http.Request) {
+	repo := AppointmentRepo{c.db.C("appointments")}
+	appointments, err := repo.All()
+	if err != nil {
+		panic(err)
+	}
+
+	WriteSuccess(w, http.StatusOK, appointments)
+}
+
+func (c *appContext) appointmentHandler(w http.ResponseWriter, r *http.Request) {
+	params := context.Get(r, "params").(httprouter.Params)
+	repo := AppointmentRepo{c.db.C("appointments")}
+	appointment, err := repo.Find(params.ByName("id"))
+	if err != nil {
+		panic(err)
+	}
+
+	WriteSuccess(w, http.StatusOK, appointment)
+}
+
+func (c *appContext) createAppointmentHandler(w http.ResponseWriter, r *http.Request) {
+	body := context.Get(r, "body").(*AppointmentResource)
+	repo := AppointmentRepo{c.db.C("appointments")}
+	err := repo.Create(&body.Data)
+	if err != nil {
+		panic(err)
+	}
+
+	WriteSuccess(w, http.StatusCreated, body)
+}
+
+func (c *appContext) updateAppointmentHandler(w http.ResponseWriter, r *http.Request) {
+	params := context.Get(r, "params").(httprouter.Params)
+	body := context.Get(r, "body").(*AppointmentResource)
+	body.Data.Id = bson.ObjectIdHex(params.ByName("id"))
+	repo := AppointmentRepo{c.db.C("appointments")}
+	err := repo.Update(&body.Data)
+	if err != nil {
+		panic(err)
+	}
+
+	WriteSuccess(w, http.StatusAccepted, body)
+}
+
+func (c *appContext) deleteAppointmentHandler(w http.ResponseWriter, r *http.Request) {
+	params := context.Get(r, "params").(httprouter.Params)
+	repo := AppointmentRepo{c.db.C("appointments")}
+	err := repo.Delete(params.ByName("id"))
+	if err != nil {
+		panic(err)
+	}
+
+	data := MessageSuccess{MessageInfo{Message: "Appointment has been deleted successfully"}}
+	WriteSuccess(w, http.StatusAccepted, data)
+}
+
+func (c *appContext) searchAppointmentsHandler(w http.ResponseWriter, r *http.Request) {
+	params := context.Get(r, "params").(httprouter.Params)
+	roomIds := r.URL.Query()["room_ids[]"]
+	owner, _ := strconv.ParseBool(params.ByName("owner"))
+	guestId := params.ByName("guest_id")
+	repo := AppointmentRepo{c.db.C("appointments")}
+	appointments, err := repo.Search(roomIds, owner, guestId)
+	if err != nil {
+		panic(err)
+	}
+
+	WriteSuccess(w, http.StatusOK, appointments)
+}
+
 func main() {
 	gotenv.Load()
 
@@ -485,6 +665,13 @@ func main() {
 	router.Delete("/rooms/:id", commonHandlers.ThenFunc(appC.deleteRoomHandler))
 	router.Get("/rooms", commonHandlers.ThenFunc(appC.roomsHandler))
 	router.Post("/rooms", commonHandlers.Append(contentTypeHandler, bodyHandler(RoomResource{})).ThenFunc(appC.createRoomHandler))
+
+	router.Get("/appointments/:id", commonHandlers.ThenFunc(appC.appointmentHandler))
+	router.Patch("/appointments/:id", commonHandlers.Append(contentTypeHandler, bodyHandler(AppointmentResource{})).ThenFunc(appC.updateAppointmentHandler))
+	router.Delete("/appointments/:id", commonHandlers.ThenFunc(appC.deleteAppointmentHandler))
+	router.Get("/appointments", commonHandlers.ThenFunc(appC.appointmentsHandler))
+	router.Post("/appointments", commonHandlers.Append(contentTypeHandler, bodyHandler(AppointmentResource{})).ThenFunc(appC.createAppointmentHandler))
+	router.Get("/search-appointments", commonHandlers.ThenFunc(appC.searchAppointmentsHandler))
 
 	port := os.Getenv("PORT")
 	msg := fmt.Sprintf("Listening at port %s", port)
